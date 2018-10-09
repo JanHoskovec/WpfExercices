f49aad9a810b60ddf4c9720f6f31fcf63d9f11d6
JanHoskovec <jean.hoskovec@gmail.com>
2018-10-09 17:28:30 +0200

    Nightly save

---
+++ Calculator/Core.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Core
+    {
+        private double _first = 0;
+        private double _second = 0;
+        private char? _operand = null;
+
+        public string AddDigit(int digit)
+        {
+            if (digit < 0 || digit > 9)
+                throw new ArgumentOutOfRangeException();
+            string result = "";
+            if(_operand == null)
+            { 
+                _first = 10 * _first + digit;
+                result = _first.ToString();
+            }
+            else
+            {
+                _second = 10 * _second + digit;
+                result = _second.ToString();
+            }
+            return result;
+        }
+
+        public char SetOperand(char operand)
+        {
+            if (operand == '+' || operand == '-' || operand == '*' || operand == '/')
+                _operand = operand;
+            else
+                throw new ArgumentException();
+            return (char)_operand;
+        }
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Core _core = new Core();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Grid _grid = new Grid();
+            this.Content = _grid;
+        }
+
+        private void ClickOnNumber(object sender, RoutedEventArgs e)
+        {
+            int.TryParse((sender as Button).Content.ToString(), out int i);
+            _core.AddDigit(i); //TODO what to do with the return ?
+        }
+
+        private void ClickOnOperator(object sender, RoutedEventArgs e)
+        {
+            _core.SetOperand((sender as Button).Content.ToString()[0]);
+        }
+
+        private void ClickOnClear(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClickOnEquals(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
