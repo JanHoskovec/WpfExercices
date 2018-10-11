46c17760d38d7d91bd57ca3f36b0e147fa83c769
JanHoskovec <jean.hoskovec@gmail.com>
2018-10-11 13:30:59 +0200

    More binding exercises

---
+++ TestLayout/Models/PersonData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLayout.Models
+{
+    public class PersonData
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get; set; }
+
+        public string MyString { get { return Nom + " " + Prenom + ", aged " + Age; } }
+
+        public PersonData()
+        {
+            Nom = "Hoskovec";
+            Prenom = "Jan";
+            Age = 29;
+        }
+    }
+}
